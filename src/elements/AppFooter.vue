f7d22f82361a281ffa0f321b0137c85060ca7708
jtreeves <jackson.t.reeves@gmail.com>
2022-03-11 20:01:59 -0500

    [elements] Rename components to elements for clarity

---
+++ src/elements/AppFooter.vue
@@ -0,0 +1,38 @@
+<template>
+    <footer>
+        © {{ year }}
+    </footer>
+</template>
+
+<script lang='ts'>
+    import {
+        computed,
+        ComputedRef
+    } from 'vue'
+
+    export default {
+        setup() {
+            const year: ComputedRef<number> = computed(
+                (): number => {
+                    const now: Date = new Date()
+                    const currentYear: number = now.getFullYear()
+                    return currentYear
+                }
+            )
+            return { year }
+        }
+    }
+</script>
+
+<style scoped>
+    footer {
+        display: flex;
+        color: #ccc;
+        justify-content: center;
+        width: 100%;
+        margin-top: 50px;
+        margin-bottom: 50px;
+        font-size: 13px;
+        line-height: 13px;
+    }
+</style>
---
+++ src/elements/AppHeader.vue
@@ -0,0 +1,71 @@
+<template>
+    <header>
+        <router-link to='/home'>
+            <img
+                alt='Rubiks Cube'
+                src='@/assets/rubiks.jpg'
+            >
+        </router-link>
+
+        <router-link to='/home'>
+            <span>
+                Learn Math
+            </span>
+        </router-link>
+
+        <nav>
+            <ul>
+                <li>
+                    <router-link to='/home'>
+                        Home
+                    </router-link>
+                </li>
+
+                <li>
+                    <router-link to='/about'>
+                        About
+                    </router-link>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+    header {
+        display: flex;
+        border-bottom: 1px solid #ccc;
+        padding: 0 20px;
+        align-items: center;
+        height: 100px;
+        gap: 20px;
+    }
+
+    img {
+        height: 50px;
+        width: 50px;
+        border-radius: 50%;
+    }
+
+    span {
+        font-size: 50px;
+    }
+
+    nav {
+        margin-left: auto;
+    }
+
+    ul {
+        list-style: none;
+        display: flex;
+        gap: 10px;
+    }
+
+    ul li {
+        display: inline-flex;
+    }
+
+    a:hover {
+        font-weight: bold;
+    }
+</style>
