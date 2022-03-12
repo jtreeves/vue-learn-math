1f8929e158c6efe29d43080fd68677df0ddab0a7
jtreeves <jackson.t.reeves@gmail.com>
2022-03-11 20:02:59 -0500

    [json] Install additional packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,10 @@ module.exports = {
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-        indent: 0
+        indent: 0,
+        '@typescript-eslint/no-empty-function': 0,
+        'space-before-function-paren': 0,
+        'no-trailing-spaces': 0
     },
     overrides: [
         {
