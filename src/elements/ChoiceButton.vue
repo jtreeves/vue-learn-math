9ad7fb42d1709444c5b064f7e96cbef5a3e5f6b2
jtreeves <jackson.t.reeves@gmail.com>
2022-03-12 16:08:23 -0500

    [ChoiceButton] Create component to display each answer choice as a button

---
+++ src/elements/ChoiceButton.vue
@@ -0,0 +1,29 @@
+<template>
+    <li>
+        <button>
+            {{ choice }}
+        </button>
+    </li>
+</template>
+
+<script lang='ts'>
+    export default {
+        props: {
+            choice: Number
+        }
+    }
+</script>
+
+<style scoped>
+    button {
+        width: 200px;
+        height: 50px;
+        padding: 7px 11px;
+        border-radius: 4px;
+        border: none;
+        background-color: darkblue;
+        color: white;
+        cursor: pointer;
+        font-size: 13px;
+    }
+</style>
