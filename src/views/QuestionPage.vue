a5389d6820641edcda1bdcafb3093708386c196c
jtreeves <jackson.t.reeves@gmail.com>
2022-03-11 23:45:10 -0500

    [QuestionPage] Generate question, then display it and its answers

---
+++ src/views/QuestionPage.vue
@@ -0,0 +1,57 @@
+<template>
+    <main>
+        <h1>Question</h1>
+        <h2>{{ question }}</h2>
+        <p>{{ answer }}</p>
+
+        <ul>
+            <li>{{ choices[0] }}</li>
+            <li>{{ choices[1] }}</li>
+            <li>{{ choices[2] }}</li>
+            <li>{{ choices[3] }}</li>
+        </ul>
+    </main>
+</template>
+
+<script lang='ts'>
+    import {
+        computed,
+        ComputedRef
+    } from 'vue'
+    import {
+        IQuestion
+    } from '@/interfaces'
+    import generateQuestion from '@/utilities/generateQuestion'
+
+    export default {
+        setup() {
+            const questionSet: IQuestion = generateQuestion(1, 'addition')
+
+            const question: ComputedRef<string> = computed(
+                (): string => {
+                    const thisQuestion: string = questionSet.question
+                    
+                    return thisQuestion
+                }
+            )
+
+            const answer: ComputedRef<number> = computed(
+                (): number => {
+                    const thisAnswer: number = questionSet.answer
+
+                    return thisAnswer
+                }
+            )
+
+            const choices: ComputedRef<number[]> = computed(
+                (): number[] => {
+                    const thisChoices: number[] = questionSet.choices
+
+                    return thisChoices
+                }
+            )
+            
+            return { question, answer, choices }
+        }
+    }
+</script>
