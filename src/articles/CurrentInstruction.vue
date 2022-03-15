be03f9e7767f1f1dab63d824d173d831384de929
jtreeves <jackson.t.reeves@gmail.com>
2022-03-14 22:06:12 -0400

    [articles] Move steps to articles

---
+++ src/articles/CurrentInstruction.vue
@@ -0,0 +1,20 @@
+<template>
+    <article>
+        <FirstInstruction v-if="props.step === '1'" />
+        <SecondInstruction v-if="props.step === '2'" />
+        <ThirdInstruction v-if="props.step === '3'" />
+    </article>
+</template>
+
+<script setup lang='ts'>
+    import { 
+        ExtractedProps 
+    } from '@/types'
+    import FirstInstruction from './FirstInstruction.vue'
+    import SecondInstruction from './SecondInstruction.vue'
+    import ThirdInstruction from './ThirdInstruction.vue'
+
+    const props: ExtractedProps = defineProps({
+        step: String
+    })
+</script>
