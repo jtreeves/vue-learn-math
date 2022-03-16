9df5c39bc8b2915e319a7dfdb0532777ea1ab3a9
jtreeves <jackson.t.reeves@gmail.com>
2022-03-15 21:26:23 -0400

    [vue.config.ts] Remove unnecessary plugin reference

--- vue.config.ts
+++ vue.config.ts
@@ -1,15 +1,7 @@
 import { defineConfig } from '@vue/cli-service'
 
 const config = defineConfig({
-    transpileDependencies: true,
-    chainWebpack: config => {
-        config
-            .plugin('html')
-            .tap(args => {
-                args[0].title = 'Learn Math'
-                return args
-            })
-    }
+    transpileDependencies: true
 })
 
 export default config
