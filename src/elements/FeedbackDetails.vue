0ccbb5bf114eae8f956f0fbcfff7259444246381
jtreeves <jackson.t.reeves@gmail.com>
2022-03-15 21:27:44 -0400

    [FeedbackDetails] Display answer and whether or not user got it correct

---
+++ src/elements/FeedbackDetails.vue
@@ -0,0 +1,26 @@
+<template>
+    <article>
+        <p>Your answer was: {{ props.selection }}</p>
+        <p>The correct answer is: {{ props.answer }}</p>
+        <p>You were {{ props.correct ? 'correct' : 'incorrect' }}</p>
+
+        <GenericButton 
+            text="Next"
+            @click="emit('getQuestion')"
+        />
+    </article>
+</template>
+
+<script setup lang="ts">
+    import GenericButton from './GenericButton.vue'
+
+    const props = defineProps<{
+        answer: number
+        selection: number
+        correct: boolean
+    }>()
+
+    const emit = defineEmits<{
+        (event: 'getQuestion'): void
+    }>()
+</script>
