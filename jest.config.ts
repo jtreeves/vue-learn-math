ddccbe948fb519a6834e7cf568e8d84e7741041d
jtreeves <jackson.t.reeves@gmail.com>
2022-03-15 21:27:00 -0400

    [jest.config.ts] Ensure code coverage tracked

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,7 @@ import { Config } from '@jest/types'
 
 const config: Config.InitialOptions = {
     verbose: true,
+    collectCoverage: true,
     preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
     testMatch: ['**/tests/**/*.spec.[jt]s?(x)'],
     setupFilesAfterEnv: ['<rootDir>/tests/index.ts']
