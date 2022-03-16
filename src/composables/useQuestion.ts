27da31421b402c184e6008b74d71e1bc9d1e720a
jtreeves <jackson.t.reeves@gmail.com>
2022-03-15 21:32:01 -0400

    [useQuestion] Add answer, selection, wasAnswered, and wasCorrect state; create showAnswer method

--- src/composables/useQuestion.ts
+++ src/composables/useQuestion.ts
@@ -3,18 +3,27 @@ import {
     Ref 
 } from 'vue'
 import { 
+    IAnswer,
     IQuestion, 
     QuestionComposable 
 } from '@/interfaces'
 import generateQuestion from '@/utilities/generateQuestion'
 import shuffleChoices from '@/utilities/shuffleChoices'
 
-function useQuestions(): QuestionComposable {
-    const initialSet: IQuestion = generateQuestion(1, 'addition')
+function useQuestion(): QuestionComposable {
+    const level: Ref<number> = ref(1)
+    const type: Ref<string> = ref('addition')
+    const selectedChoice: Ref<number> = ref(0)
+    const wasAnswered: Ref<boolean> = ref(false)
+    const wasCorrect: Ref<boolean> = ref(false)
+    const answerHistory: Ref<IAnswer[]> = ref([])
+
+    const initialSet: IQuestion = generateQuestion(level.value, type.value)
 
     const question: Ref<string> = ref(initialSet.question)
     const choices: Ref<number[]> = ref(initialSet.choices)
     const shuffledChoices: Ref<number[]> = ref(shuffleChoices(choices.value))
+    const correctAnswer: Ref<number> = ref(choices.value[0])
 
     function determineCorrect(
         answer: number
@@ -27,19 +36,45 @@ function useQuestions(): QuestionComposable {
     }
 
     function updateQuestion(): void {
+        level.value = 1
+        type.value = 'addition'
+        
         const updatedSet: IQuestion = generateQuestion(1, 'addition')
-
+        
         question.value = updatedSet.question
         choices.value = updatedSet.choices
         shuffledChoices.value = shuffleChoices(updatedSet.choices)
+        correctAnswer.value = updatedSet.choices[0]
+        wasAnswered.value = false
+    }
+
+    function showAnswer(
+        choice: number,
+        correct: boolean
+    ): void {
+        const answer: IAnswer = {
+            type: type.value,
+            level: level.value,
+            correct: correct
+        }
+
+        wasAnswered.value = true
+        wasCorrect.value = correct
+        selectedChoice.value = choice
+        answerHistory.value.push(answer)
     }
 
     return { 
         question,
         choices: shuffledChoices,
+        answer: correctAnswer,
+        selection: selectedChoice,
+        wasAnswered,
+        wasCorrect,
         determineCorrect,
-        updateQuestion
+        updateQuestion,
+        showAnswer
     }
 }
 
-export default useQuestions
+export default useQuestion
